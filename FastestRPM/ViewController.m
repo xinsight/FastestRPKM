8e550c7aecb56d411c9b3848d79df940d3858393
Jay Moore <jmoore@ehealthinnovation.org>
2015-08-18 20:50:51 -0400

    fix math

--- FastestRPM/ViewController.m
+++ FastestRPM/ViewController.m
@@ -15,14 +15,24 @@
 
 @end
 
+CGFloat min = M_PI - M_PI_4; // ~2.355
+CGFloat max = M_PI_4; // ~0.785
+
 @implementation ViewController
-
-
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.needle.transform = CGAffineTransformRotate(self.needle.transform, M_PI/ 180 * -225);
+    // 0..2π (6.28)
+    //
+//    self.needle.transform = CGAffineTransformRotate(self.needle.transform, M_PI/ 180 * -225);
+    [self resetNeedle];
+}
+
+- (void) resetNeedle;
+{
+    // !!!: set the transform to identity and use CGAffineTransformRotate, or use CGAffineTransformMakeRotation()
+    self.needle.transform = CGAffineTransformMakeRotation(min);
 }
 
 - (void)didReceiveMemoryWarning {
@@ -31,21 +41,36 @@
 }
 
 -(IBAction)panViewGesture:(UIPanGestureRecognizer *)recognizer {
+    
+//    return;
+    
    CGPoint velocityPoint = [recognizer velocityInView:self.view];
     float velocity = sqrt( ( pow(velocityPoint.x, 2) + pow(velocityPoint.y,2) ) );
+    
     float velocityPercent = velocity/1000;
-    if (velocityPercent > 1) {
-        velocityPercent = 1;
+    if (velocityPercent > 1.0) {
+        velocityPercent = 1.0;
     }
+    
+    // clamp
+    velocityPercent = MIN(velocityPercent, 1.0);
+    velocityPercent = MAX(velocityPercent, 0.0);
     
     self.printSpeed.text = [NSString stringWithFormat:@"%f",velocityPercent];
     
     NSLog(@"%f", velocity);
     
+//    CGFloat angle = min + ((max - min) * velocityPercent);
+    CGFloat angle = min + ((max + min) * velocityPercent);
+    
     [UIView animateWithDuration:0.2 animations:^{
-    self.needle.transform = CGAffineTransformMakeRotation(M_PI/180 * (velocityPercent *270) -225);
+//        self.needle.transform = CGAffineTransformMakeRotation(M_PI/180 * (velocityPercent *270) -225);
+        self.needle.transform = CGAffineTransformMakeRotation(angle);
     
         if (recognizer.state == UIGestureRecognizerStateEnded ) {
+            
+            [self.needle.layer removeAllAnimations];
+            
             NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(cancelTimer:) userInfo:nil repeats:NO];
         }
     
@@ -58,8 +83,9 @@
 
 -(void)cancelTimer:(NSTimer *)timer {
     [UIView animateWithDuration:1.5 animations:^{
-        self.needle.transform = CGAffineTransformMakeRotation(M_PI/180 * -225);
-    
+//        self.needle.transform = CGAffineTransformMakeRotation(M_PI/180 * -225);
+ 
+        [self resetNeedle];
     }];
 }
 
